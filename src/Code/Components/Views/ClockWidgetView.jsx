b92fe7164f0f86c2304b303c0c13d2169a4026b1
jweatherspoon <jweatherspoon@nevada.unr.edu>
2019-05-29 21:08:32 -0700

    boilerplate react app

---
+++ src/Code/Components/Views/ClockWidgetView.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ClockWidgetView = (_) => {
+    
+    
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ClockWidgetView;
---
+++ src/Code/Components/Views/WeatherWidgetView.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const WeatherWidgetView = (_) => (
+    <div></div>
+);
+
+export default WeatherWidgetView;
