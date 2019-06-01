82a1572abc2ad1fc96ed270e0ac9fd964234fd8f
jweatherspoon <jweatherspoon@nevada.unr.edu>
2019-05-31 21:42:32 -0700

    Update to ci config. Added failing test

--- src/Tests/ComponentTests/App.test.js
+++ src/Tests/ComponentTests/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../Code/Components/Views/App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App view tests.", () => {
+    test('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test("Test that fails.", () => expect(false).toBe(true));
 });
