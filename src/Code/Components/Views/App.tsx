17118a38e05e79649986e5c6a516321abe098c2a
jweatherspoon <jweatherspoon@nevada.unr.edu>
2019-06-01 00:48:46 -0700

    styling

--- src/Code/Components/Views/App.tsx
+++ src/Code/Components/Views/App.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import WeatherWidgetView from './WeatherWidgetView';
 import ClockWidgetView from './ClockWidgetView';
 
+import {
+    Container, 
+    Row, 
+    Col
+} from './BootstrapComponents';
+
 /**
  * Application entry point
  */
 const App = () => {
   return (
-    <div className="App">
-      <ClockWidgetView />
-    </div>
+    <Container id="container" >
+        <Row>
+            <Col style={{float: "right"}}>
+                <ClockWidgetView />
+            </Col>
+        </Row>
+    </Container>
   );
 }
 
